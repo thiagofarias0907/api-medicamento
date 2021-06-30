e8e961982899b83969c1ab3f7937f8e82801432e
thiagofeffecti <thiago.farias@effecti.com.br>
2021-06-30 00:09:11 -0300

    Add: Basic Spring App

---
+++ src/test/java/br/udesc/ppr/apimedicamento/ApiMedicamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.udesc.ppr.apimedicamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedicamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
